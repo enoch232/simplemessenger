144cd7012311c6508490559f3860424ae3c19887
enoch232 <enoch232@gmail.com>
2016-04-14 16:48:48 -0700

    make chatting a bit functional

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,25 @@
 class ConversationsController < ApplicationController
+	before_action :authenticate_user!
   def index
-  	@conversations = Conversation.all
+  	@conversations = Conversation.all.where('sender_id=? OR receiver_id=?',current_user,current_user)
   end
 
   def new
+  	@conversation = Conversation.new
+
+  end
+  def create
+  	@conversation = Conversation.new
+  	@conversation.receiver_id = User.all.where('email = ?', params[:receiver_email]).first.id
+  	@conversation.sender_id = current_user.id
+  	if @conversation.save
+  		redirect_to root_path
+  	else
+  		render :new
+  	end
+  end
+  private
+  def conversation_params
+  	#params.require(:conversation).permit(:receiver_id, :sender_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  resources :conversations
   root 'conversations#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
